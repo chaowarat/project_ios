10071c6840e902159fd4d4c2d598102766446f4b
student <student@CSIT-iMac-41.local>
2012-09-04 09:21:38 +0700

    add empty project

---
+++ EmptyTabbedApplication/EmptyTabbedApplication/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  EmptyTabbedApplication
+//
+//  Created by student on 9/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ EmptyTabbedApplication/EmptyTabbedApplication/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  EmptyTabbedApplication
+//
+//  Created by student on 9/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
