e8825a8d3d13271a845ce359a7a126a5749a91e3
chaowarat <chaowarut@hotmail.com>
2012-09-17 16:54:38 +0700

    new table view(cute)

--- CarPrice2/CarPrice/RootViewController.m
+++ CarPrice2/CarPrice/RootViewController.m
@@ -36,6 +36,7 @@
     [super viewDidLoad];
     self.navigationItem.title = @"Car Price";
     self.tableView.backgroundColor = [UIColor grayColor];
+    self.tableView.separatorStyle = NO;
 }
 
 -(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
@@ -76,12 +77,33 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    UILabel *topLabel;
     static NSString *CellIdentifier = @"CellIdentifier";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if(cell == nil){
         cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        
+        topLabel =[[[UILabel alloc] initWithFrame:CGRectMake(20,10,300,20)]autorelease];
+		[cell.contentView addSubview:topLabel];
+        
+		topLabel.backgroundColor = [UIColor clearColor];
+		topLabel.textColor = [UIColor colorWithRed:0.25 green:0.0 blue:0.0 alpha:1.0];
+		topLabel.highlightedTextColor = [UIColor colorWithRed:1.0 green:1.0 blue:0.9 alpha:1.0];
+		topLabel.font = [UIFont systemFontOfSize:30];
+        
+		// Create a background image view.
+		cell.backgroundView =
+        [[[UIImageView alloc] init] autorelease];
+		cell.selectedBackgroundView =
+        [[[UIImageView alloc] init] autorelease];
     }
+    else
+	{
+		topLabel = (UILabel *)[cell viewWithTag:10];
+	}
+    
     
     Car *car = [[Car alloc] init];
     if (indexPath.section == 0) {
@@ -90,10 +112,36 @@
     else{
         car = [listBigPrice objectAtIndex:indexPath.row];
     }
-    cell.textLabel.font = [UIFont fontWithName:@"Chalkboard SE" size:20];
-    cell.textLabel.text = [NSString stringWithFormat:@"%@ %@", car.manufacture, car.model];
-
+    topLabel.font = [UIFont fontWithName:@"Chalkboard SE" size:20];
+    topLabel.text = [NSString stringWithFormat:@"%@ %@", car.manufacture, car.model];
     
+    UIImage *rowBackground;
+	UIImage *selectionBackground;
+	NSInteger sectionRows = [tableView numberOfRowsInSection:[indexPath section]];
+	NSInteger row = [indexPath row];
+	if (row == 0 && row == sectionRows - 1)
+	{
+		rowBackground = [UIImage imageNamed:@"topAndBottomRow.png"];
+		selectionBackground = [UIImage imageNamed:@"topAndBottomRowSelected.png"];
+	}
+	else if (row == 0)
+	{
+		rowBackground = [UIImage imageNamed:@"topRow.png"];
+		selectionBackground = [UIImage imageNamed:@"topRowSelected.png"];
+	}
+	else if (row == sectionRows - 1)
+	{
+		rowBackground = [UIImage imageNamed:@"bottomRow.png"];
+		selectionBackground = [UIImage imageNamed:@"bottomRowSelected.png"];
+	}
+	else
+	{
+		rowBackground = [UIImage imageNamed:@"middleRow.png"];
+		selectionBackground = [UIImage imageNamed:@"middleRowSelected.png"];
+	}
+	((UIImageView *)cell.backgroundView).image = rowBackground;
+	((UIImageView *)cell.selectedBackgroundView).image = selectionBackground;
+	
     return cell;
 }
 
